0ed7774220d0daeb10ed68a571f2bef8290d4c65
Ninad Parab <42116222+k33da-lets-debug@users.noreply.github.com>
2020-11-17 19:00:30 +0530

    Image preprocessing
    Added image preprocessing

--- backend/rectex/image_transformation_utils.py
+++ backend/rectex/image_transformation_utils.py
@@ -93,4 +93,26 @@ def remove_noise(img):
     return res
 
 
+def remove_lines(img):
+    result = img.copy()
+    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    # Remove horizontal lines
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (150,1))
+    remove_horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+    cnts = cv2.findContours(remove_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(result, [c], -1, (255,255,255), 5)
+
+    # Remove vertical lines
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,150))
+    remove_vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
+    cnts = cv2.findContours(remove_vertical, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(result, [c], -1, (255,255,255), 5)
+
+    return result
+
 
--- backend/rectex/views.py
+++ backend/rectex/views.py
@@ -4,51 +4,62 @@ from PIL import Image
 import pytesseract
 import cv2
 import numpy as np
-from .image_transformation_utils import compute_skew,deskew,skeletonize,remove_noise
+from .image_transformation_utils import compute_skew,deskew,skeletonize,remove_noise,remove_lines
 # Create your views here.
 
 def rectex_view(request):
 
     #TODO:
     # 1. Get image
-    raw_image = cv2.imread('staticroot/test_ocr_images/Sample712.png')
-
+    raw_image = cv2.imread('staticroot/test_ocr_images/Test.jpg')
     # 2. Convert it to rgb
     rgb_image = cv2.cvtColor(raw_image,cv2.COLOR_BGR2RGB)
+    # Convert it to greyscale
+    grayscale_image = cv2.cvtColor(rgb_image,cv2.COLOR_RGB2GRAY)  
+    # 2.1 binarization: converting to black and white pixels, and removing watermark
+    # define a threshold, 128 is the middle of black and white in grey scale
+    # threshold the image
+    thresh = 128
+    img_binary = cv2.threshold(grayscale_image, thresh, 255, cv2.THRESH_BINARY)[1] 
 
-    # 2.1 binarization: converting to black and white pixels
-    grayscale_image = cv2.cvtColor(rgb_image,cv2.COLOR_RGB2GRAY)    
+    #Text thickning
+    kernel = np.ones((1,1), np.uint8) 
+    text_thickning = cv2.erode(img_binary,kernel,iterations = 2)
+    cv2.imwrite('staticroot/test_ocr_images/threshresult.png',text_thickning)
 
-    #Remove noise
-    adaptive_threshold = cv2.adaptiveThreshold(grayscale_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,81,10)
-    #Sharpen text
-    adaptive_threshold = cv2.adaptiveThreshold(adaptive_threshold, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 4)
+    #Removing H/V lines
+    removed_lines = remove_lines(text_thickning)
+    cv2.imwrite('staticroot/test_ocr_images/removed_lines.png',removed_lines)
 
-    cv2.imwrite('staticroot/test_ocr_images/threshresult.png',adaptive_threshold)
-
-    # 2.2 Noise removal: Removing patches, dots etc
-    # noise_removed = remove_noise(adaptive_threshold)
-    # cv2.imwrite('staticroot/test_ocr_images/noiseremovalresult.png',noise_removed)
-
-    # 2.3 Skew correction: Setting orientation (Hough transformation)
+    # Skew correction: Setting orientation (Hough transformation)
     # computed_skew = compute_skew(adaptive_threshold)
     # deskewed = deskew(adaptive_threshold,computed_skew)
     # cv2.imwrite('staticroot/test_ocr_images/dskewedresult.png',deskewed)
 
-
-    # 2.4 Thinning and Skeletonization: 
+    # Thinning and Skeletonization: 
     # skeletonized = skeletonize(background_noise_reduced)
     # cv2.imwrite('staticroot/test_ocr_images/skeletonizationresult.png',skeletonized)
 
-    # 3. Apply some filters
+    # Text recognization using tessaract and Pytessract
+    # hImg,wImg= removed_lines.shape
+    # detected_text = pytesseract.image_to_data(removed_lines,lang='script/Devanagari')
+    # #print(detected_text)
 
+    # bound_result = None
+    # for x,b in enumerate(detected_text.splitlines()):
+    #     if x!=0:
+    #         b = b.split() 
+    #         if len(b)==12:
+    #             a,b,w,h = int(b[6]),int(b[7]) ,int(b[8]) ,int(b[9]) 
+    #             bound_result = cv2.rectangle(removed_lines,(a,b),(w+a,h+b),(0,0,255),3)
+    #             # cv2.puttext(img_erosion,b[0],(a,hImg-b+25),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
 
-    # 4. Recognize text
-
-    # 5. Add bounded boxes, confidance code to recgonized text
+    # # 5. Add bounded boxes, confidance code to recgonized text
+    # cv2.imwrite('staticroot/test_ocr_images/boundresult.png',bound_result)
 
     # 6. Display text and image with recognized text
 
     # 7. Do some NLP magic
 
-    return HttpResponse('<h1>'+str(pytesseract.image_to_string(adaptive_threshold,lang='script/Devanagari'))+'</h1>')
+
+    return HttpResponse('<h1>'+str(pytesseract.image_to_string(removed_lines,lang='script/Devanagari'))+'</h1>')
