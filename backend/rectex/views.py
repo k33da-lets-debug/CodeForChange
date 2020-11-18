2686bbc4900c6c9abef405a8590f11ccbd770f3e
Ninad Parab <42116222+k33da-lets-debug@users.noreply.github.com>
2020-11-18 15:59:01 +0530

    Added media layer

--- backend/rectex/views.py
+++ backend/rectex/views.py
@@ -84,34 +84,49 @@ def rectex_view(request):
             #TODO: Optimise this pipeline
             data = OCR.objects.get(id=d.id)
             url = data.to_be_converted_image.url[1:]
+            file_meta =url.split('/')[-1]
+            filename = file_meta.split('.')[0]
+
 
             raw_image = Image.open(url)
             opencv_image = cv2.cvtColor(np.array(raw_image), cv2.COLOR_RGB2BGR)
             
             #conversion 1
             rgb_image = cv2.cvtColor(opencv_image,cv2.COLOR_BGR2RGB)
-            cv2.imwrite('media/converted/cv1.png',rgb_image)
+            tempu = 'media/converted/'+filename+'_'+'cv1.png'
+            context['cv1_url'] = tempu
+            cv2.imwrite(tempu,rgb_image)
             
             #Conversion 2
             grayscale_image = cv2.cvtColor(rgb_image,cv2.COLOR_RGB2GRAY)
-            cv2.imwrite('media/converted/cv2.png',grayscale_image)
+            tempu = 'media/converted/'+filename+'_'+'cv2.png'
+            context['cv2_url'] = tempu
+            cv2.imwrite(tempu,grayscale_image)
             
             #Conversion 3
             binary_image = cv2.threshold(grayscale_image, 128, 255, cv2.THRESH_BINARY)[1] 
-            cv2.imwrite('media/converted/cv3.png',binary_image)
+            tempu = 'media/converted/'+filename+'_'+'cv3.png'
+            context['cv3_url'] = tempu
+            cv2.imwrite(tempu,binary_image)
 
             #Conversion 4
-            kernel = np.ones((1,1), np.uint8) 
-            text_thickned_image = cv2.erode(binary_image,kernel,iterations = 2)
-            cv2.imwrite('media/converted/cv4.png',text_thickned_image)
+            kernel = np.ones((2,2), np.uint8) 
+            text_thickned_image = cv2.erode(binary_image,kernel,iterations = 1)
+            tempu = 'media/converted/'+filename+'_'+'cv4.png'
+            context['cv4_url'] = tempu
+            cv2.imwrite(tempu,text_thickned_image)
 
             #Conversion 5
             removed_horizontal_lines_image = remove_horizontal_lines(text_thickned_image)
-            cv2.imwrite('media/converted/cv5.png',removed_horizontal_lines_image)
+            tempu = 'media/converted/'+filename+'_'+'cv5.png'
+            context['cv5_url'] = tempu
+            cv2.imwrite(tempu,removed_horizontal_lines_image)
 
             #Conversion 6
             removed_verticle_lines_image = remove_verticle_lines(removed_horizontal_lines_image)
-            cv2.imwrite('media/converted/cv6.png',removed_verticle_lines_image)
+            tempu = 'media/converted/'+filename+'_'+'cv6.png'
+            context['cv6_url'] = tempu
+            cv2.imwrite(tempu,removed_verticle_lines_image)
 
             #Conversion N
             # There are some more conversions 
@@ -119,29 +134,25 @@ def rectex_view(request):
             # skeletonize_image = skeletonize(removed_horizontal_lines_image)
             # cv2.imwrite('media/converted/cv7.png',skeletonize_image)
 
-            denoise_image = denoise(removed_verticle_lines_image)
-            cv2.imwrite('media/converted/cvN.png',denoise_image)
+            # denoise_image = denoise(removed_verticle_lines_image)
+            # cv2.imwrite('media/converted/cvN.png',denoise_image)
 
             #End of dummy conversions
 
             #Extracted data TODO: Improve performance
-            ocr_text=str(pytesseract.image_to_string(removed_verticle_lines_image,lang='script/Devanagari'))
+            ocr_text=str(pytesseract.image_to_string(removed_verticle_lines_image,lang='script/Devanagari',config='--oem 3 --psm 6'))
+            context['result'] = ocr_text
+
             #Bounded rectange
             bounded_rectangle_image = get_bounded_rectangles_on_identified_text(removed_verticle_lines_image)
-            cv2.imwrite('media/converted/brres.png',bounded_rectangle_image)
+            tempu = 'media/converted/'+filename+'_'+'brres.png'
+            context['brres_url'] = tempu
+            cv2.imwrite(tempu,bounded_rectangle_image)
             
     
-            #Data formation : TODO: Improve code
-            context['cv1_url'] = '/media/converted/cv1.png'
-            context['cv2_url'] = '/media/converted/cv2.png'
-            context['cv3_url'] = '/media/converted/cv3.png'
-            context['cv4_url'] = '/media/converted/cv4.png'
-            context['cv5_url'] = '/media/converted/cv5.png'
-            context['cv6_url'] = '/media/converted/cv6.png'
-            context['brres_url'] = '/media/converted/cv2.png'
-            context['conversion_result'] = '/media/converted/brres.png'
-            context['result'] = ocr_text
+            
 
     context['form'] = form
     return render(request,'rectex/rectex_template.html',context=context)
+
 
